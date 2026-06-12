220d43f5887f0364f0152ba1b3b0cb7953e4bff3
yoi20 <joel.amaldas@gmail.com>
2026-06-12 16:12:47 -0600

    redid gooey

--- gui.py
+++ gui.py
@@ -1,23 +1,39 @@
 import customtkinter as ctk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-app = ctk.CTk()
+class ShotCalculatorGUI(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("FRC Aimbot")
+        self.geometry("1400x800")
+        ctk.set_appearance_mode("dark")
 
-app.geometry("1000x800")
+        self.sidebar = ctk.CTkFrame(self, width=350)
+        self.sidebar.pack(side = "left", fill = "y", padx = 10, pady = 10)
 
-left_frame = ctk.CTkFrame(app)
+        self.tabs = ctk.CTkTabview(self.sidebar)
+        self.tabs.pack(fill = "both", expand = True)
 
-slider_dist = ctk.CTkSlider(left_frame, from_=2.0, to=10.0, command=update_plot)
-slider_vrad = ctk.CTkSlider(left_frame, from_=-5.0, to=5.0, command=update_plot)
-slider_vtan = ctk.CTkSlider(left_frame, from_=-5.0, to=5.0, command=update_plot)
-slider_pitch = ctk.CTkSlider(left_frame, from_=-5.0, to=5.0, command=update_plot)
-slider_roll = ctk.CTkSlider(left_frame, from_=-5.0, to=5.0, command=update_plot)
+        self.tabs.add("Robot")
+        self.tabs.add("Game Piece")
+        self.tabs.add("Environment")
+        self.tabs.add("Targets")
+        self.tabs.add("Sandbox")
 
 
-def update_plot(event):
-    v_rad = slider_vrad.get()
-    v_tan = slider_vtan.get()
-    pitch = slider_pitch.get()
-    roll = slider_roll.get()
+        self.plot_frame = ctk.CTkFrame(self)
+        self.plot_frame.pack(side = "right", fill = "both", expand = True, padx = 10, pady = 10)
+        self.fig = plt.Figure(figsize = (8,8), facecolor = "#2b2b2b")
+        self.ax = self.fig.add_subplot(111, projection = '3d')
+        self.ax.set_facecolor('#2b2b2b')
+        self.ax.tick_params(colors = 'white')
+        
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.plot_frame)
+        self.canvas.get_tk_widget().pack(fill = "both", expand = True)
 
-    inputs = np.array([[dist, height, v_rad, v_tan, pitch, roll]])
 
+
+if __name__ == "__main__":
+    app = ShotCalculatorGUI()
+    app.mainloop()
